d3a6257047c9c40431b203bf0f4f7ca5effa1c8e
prathamgalbale <150934471+prathamgalbale@users.noreply.github.com>
2023-11-19 22:11:36 +0530

    done styling responsive footer

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerNav = document.querySelector(".hamburgerNav");
 
-hamburger.addEventListener("click",() => {
-    hamburger.classList.toggle("active");
-    hamburgerNav.classList.toggle("active");
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  hamburgerNav.classList.toggle("active");
+});
 
 const hamburgerFooterOne = document.querySelector(".hamburgerFooterOne");
-const hamburgerFooterListOne = document.querySelector(".hamburgerFooterListOne");
+const hamburgerFooterListOne = document.querySelector(
+  ".hamburgerFooterListOne"
+);
 
-hamburgerFooterOne.addEventListener("click",() => {
-    hamburgerFooterOne.classList.toggle("active");
-    hamburgerFooterListOne.classList.toggle("active");
-})
+hamburgerFooterOne.addEventListener("click", () => {
+  hamburgerFooterOne.classList.toggle("active");
+  hamburgerFooterListOne.classList.toggle("active");
+});
 
-  
+const hamburgerFooterTwo = document.querySelector(".hamburgerFooterTwo");
+const hamburgerFooterListTwo = document.querySelector(
+  ".hamburgerFooterListTwo"
+);
+
+hamburgerFooterTwo.addEventListener("click", () => {
+  hamburgerFooterTwo.classList.toggle("active");
+  hamburgerFooterListTwo.classList.toggle("active");
+});
+
+const hamburgerFooterThree = document.querySelector(".hamburgerFooterThree");
+const hamburgerFooterListThree = document.querySelector(
+  ".hamburgerFooterListThree"
+);
+
+hamburgerFooterThree.addEventListener("click", () => {
+  hamburgerFooterThree.classList.toggle("active");
+  hamburgerFooterListThree.classList.toggle("active");
+});
+
+const hamburgerFooterFour = document.querySelector(".hamburgerFooterFour");
+const hamburgerFooterListFour = document.querySelector(
+  ".hamburgerFooterListFour"
+);
+
+hamburgerFooterFour.addEventListener("click", () => {
+  hamburgerFooterFour.classList.toggle("active");
+  hamburgerFooterListFour.classList.toggle("active");
+});
