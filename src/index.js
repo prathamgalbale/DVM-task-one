cb682eaa60b65e45e9ad8ba6e72de73af07aaea1
prathamgalbale <150934471+prathamgalbale@users.noreply.github.com>
2023-11-28 22:59:12 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const hamburgerNav = document.querySelector(".hamburgerNav");
-
-hamburger.addEventListener("click",() => {
-    hamburger.classList.toggle("active");
-    hamburgerNav.classList.toggle("active");
-})
-
-  
